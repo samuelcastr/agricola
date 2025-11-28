e03d3676c02d483a3bed87ffe18760a40e774a1d
Heidy-Vivas <hvivas2208@gmail.com>
2025-11-28 14:03:41 -0500

    Añade vista de inicio en urls.py y elimina PyMySQL de requirements.txt

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("API funcionando correctamente!")
+
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -41,6 +46,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     # Admin
+	path('', home), 
     path('admin/', admin.site.urls),
 
     # SWAGGER
